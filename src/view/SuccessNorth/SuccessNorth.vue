699c99af0d356f1ce8389892395650902972b5d7
unknown <1169470657@qq.com>
2018-09-10 18:11:59 +0800

    修改成功页

--- src/view/SuccessNorth/SuccessNorth.vue
+++ src/view/SuccessNorth/SuccessNorth.vue
@@ -4,17 +4,16 @@
     <div class="qrCodeContent">
       <img class="QRcodeImg" src="../../assets/img/QRcodeNorth.jpg" alt="404">
     </div>
-    <div class="footer">footer</div>
+    <div class="footer">识别二维码填写快递信息</div>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-    }
+  data() {
+    return {};
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -30,7 +29,7 @@ export default {
 .title {
   font-size: 6vh;
   font-weight: bold;
-
+  margin-top: 4vh;
 }
 .qrCodeContent {
   height: 40vh;
@@ -42,9 +41,12 @@ export default {
   width: 40vh;
 }
 .footer {
-  margin-top: 6vh;
+  margin-top: 3vh;
   flex: 1;
   width: 94%;
   padding: 2vh;
+  display: flex;
+  justify-content: center;
+  font-size: 2.5vh;
 }
 </style>
--- src/view/SuccessSouth/SuccessSouth.vue
+++ src/view/SuccessSouth/SuccessSouth.vue
@@ -4,7 +4,7 @@
     <div class="qrCodeContent">
       <img class="QRcodeImg" src="../../assets/img/QRcodeSouth.jpg" alt="404">
     </div>
-    <div class="footer">footer</div>
+    <div class="footer">识别二维码填写快递信息</div>
   </div>
 </template>
 
@@ -43,9 +43,12 @@ export default {
   width: 40vh;
 }
 .footer {
-  margin-top: 6vh;
+  margin-top: 3vh;
   flex: 1;
   width: 94%;
   padding: 2vh;
+  display: flex;
+  justify-content: center;
+  font-size: 2.5vh;
 }
 </style>
